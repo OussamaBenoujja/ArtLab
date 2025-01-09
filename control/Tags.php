54ad79b1c03534166f11875d5b7f7e443c700f75
OussamaBenoujja <osama2code79@gmail.com>
2025-01-09 23:36:39 +0100

    Update Tags control logic

--- control/Tags.php
+++ control/Tags.php
@@ -1,30 +1,20 @@
-
-
-
 <?php
 
-
-
-class tags{
-
-    private $name;
-    private $description;
-    private $tagID;
+class Tags {
     private $conn;
-
 
     public function __construct($db) {
         $this->conn = $db;
     }
 
-    public function getAlltags(){
+    public function getAllTags() {
         $query = "SELECT * FROM Tags";
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function getTagByID($tagID){
+    public function getTagByID($tagID) {
         $query = "SELECT * FROM Tags WHERE TagID = :tagID";
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(':tagID', $tagID);
@@ -32,25 +22,32 @@ class tags{
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function createTag($name, $description){
-        $query = "INSERT INTO Tags (Name, Description) VALUES (:name, :description)";
+    public function createTag($name, $description = null) {
+        $query = "INSERT INTO Tags (TagName, Description) VALUES (:name, :description)";
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(':name', $name);
         $stmt->bindParam(':description', $description);
         return $stmt->execute();
     }
 
-    public function updateTag($tagID, $name = null, $description = null){
-        $query = "UPDATE Tags SET Name = :name, Description = :description WHERE TagID = :tagID";
+    public function updateTag($tagID, $name, $description) {
+        $query = "UPDATE Tags SET TagName = :name, Description = :description WHERE TagID = :tagID";
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(':name', $name);
         $stmt->bindParam(':description', $description);
         $stmt->bindParam(':tagID', $tagID);
         return $stmt->execute();
     }
-    
 
+    public function deleteTag($tagID) {
+        $query = "DELETE FROM ArticleTags WHERE TagID = :tagID";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':tagID', $tagID);
+        $stmt->execute();
+
+        $query = "DELETE FROM Tags WHERE TagID = :tagID";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':tagID', $tagID);
+        return $stmt->execute();
+    }
 }
-
-
-?>
